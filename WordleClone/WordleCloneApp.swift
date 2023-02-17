0376add2d6d96b1085dd69a617257988a8acfe3b
Arthur Ford <adf.ford@gmail.com>
2023-02-17 15:39:05 -0500

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Arthur Ford on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
